aa86c00155a1738f41a4e8bbd2f24af868c1446b
JangSungwoo <JangSungwoo@host.docker.internal>
2022-12-04 10:22:30 +0900

    29221204테스트 중지 처리

--- src/test/java/com/source/plusutil/PlusUtilApplicationTests.java
+++ src/test/java/com/source/plusutil/PlusUtilApplicationTests.java
@@ -7,16 +7,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootTest
 class PlusUtilApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	public void BCryptPasswordEncoderTest() {
-		String test = "test123";
-		
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		System.out.println(passwordEncoder.encode(test));
-	}
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//	@Test
+//	public void BCryptPasswordEncoderTest() {
+//		String test = "test123";
+//		
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//		System.out.println(passwordEncoder.encode(test));
+//	}
 	
 }
--- src/test/java/com/source/plusutil/algorithm/BfsTestCase.java
+++ src/test/java/com/source/plusutil/algorithm/BfsTestCase.java
@@ -9,29 +9,29 @@ import com.source.plusutil.service.algorithmService.BfsService;
 @SpringBootTest
 public class BfsTestCase {
 
-	@Autowired
-	BfsService bfsService;
-	
-	@Test
-	public void defaultBfsTest() {
-		int[][] maps = {
-				{1,0,1,1,1}
-				,{1,0,1,0,1}
-				,{1,0,1,1,1}
-				,{1,1,1,0,1}
-				,{0,0,0,0,1}
-		};
-		
-		int startRow = 2;
-		int startCol = 3;
-		
-		int endRow = 4;
-		int endCol = 4;
-		
-		int block = 0;
-		
-		int result = -1;
-		result = bfsService.bfsDefault(maps, startRow, startCol, endRow, endCol, block);
-		System.out.println(result);
-	}
+//	@Autowired
+//	BfsService bfsService;
+//	
+//	@Test
+//	public void defaultBfsTest() {
+//		int[][] maps = {
+//				{1,0,1,1,1}
+//				,{1,0,1,0,1}
+//				,{1,0,1,1,1}
+//				,{1,1,1,0,1}
+//				,{0,0,0,0,1}
+//		};
+//		
+//		int startRow = 2;
+//		int startCol = 3;
+//		
+//		int endRow = 4;
+//		int endCol = 4;
+//		
+//		int block = 0;
+//		
+//		int result = -1;
+//		result = bfsService.bfsDefault(maps, startRow, startCol, endRow, endCol, block);
+//		System.out.println(result);
+//	}
 }
