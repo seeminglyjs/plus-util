4669c985e66b3dc688db69007355398e7930737b
JangSungwoo <JangSungwoo@host.docker.internal>
2022-12-12 20:49:15 +0900

    20221212레이아웃수정

--- src/main/resources/static/js/layoutLoad.js
+++ src/main/resources/static/js/layoutLoad.js
@@ -1,6 +1,5 @@
 $(document).ready( function() {
 					$("#header-load").load("/html/header.html");  //헤더 인클루드
-					$("#nav-load").load("/html/nav.html");  //네비게이션 인클루드
 					$("#footer-load").load("/html/footer.html");  //푸터부분 인클루드 
 								}
 					);
